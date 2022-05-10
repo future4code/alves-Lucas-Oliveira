1a24c41aaa7b8bc8b91a448832b095d6b1746ec7
Lucas-Ferreira-De-Oliveira <lucasyoshi@hotmail.com>
2022-05-10 14:27:48 -0300

    Esse-arquivo-nao-estava-salvo

---
+++ boilerplate-exercícios/boilerplate-exercícios/index.js
@@ -0,0 +1,11 @@
+console.log("Olá mundo!")
+
+const primeiroNome = "Lucas"
+const sobrenome = "Ferreira"
+let idade = "34"
+let souEstudante = true
+
+console.log ("Olá meu nome é", primeiroNome, sobrenome, "sou estudante", souEstudante)
+
+const seuNome = prompt("digite seu nome")
+const idade = prompt ("idade")
